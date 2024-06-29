a93eee392f96bf7a20b4562a19f38f39a4741540
mac <ozgr@live.com>
2024-06-29 23:28:48 +0300

    Add first and last buttons

--- components/Colors.jsx
+++ components/Colors.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import niceColors from 'nice-color-palettes'
 
+import clx from '@functions/clx'
 import ntc from '@functions/ntc'
 import styles from '@styles/Colors.module.scss'
 import adjustTextColor from '@functions/adjustTextColor'
@@ -35,8 +36,28 @@ export default () => {
             </div>
 
             {
+              key === 0 && (
+                <div className={clx(styles.newColorButtonWrapper, styles.firstButton)}>
+                  <button className={styles.newColorButton}>
+                    +
+                  </button>
+                </div>
+              )
+            }
+
+            {
               key !== colors.length - 1 && (
                 <div className={styles.newColorButtonWrapper}>
+                  <button className={styles.newColorButton}>
+                    +
+                  </button>
+                </div>
+              )
+            }
+
+            {
+              key === colors.length - 1 && (
+                <div className={clx(styles.newColorButtonWrapper, styles.lastButton)}>
                   <button className={styles.newColorButton}>
                     +
                   </button>
