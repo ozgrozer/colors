0d62949f520ab47c670809044b4aefe00f1703b7
mac <ozgr@live.com>
2024-06-29 21:24:01 +0300

    Fix color name color

--- components/Colors.jsx
+++ components/Colors.jsx
@@ -1,5 +1,6 @@
 import ntc from '@functions/ntc'
 import styles from '@styles/Colors.module.scss'
+import adjustTextColor from '@functions/adjustTextColor'
 
 export default () => {
   const colors = ['#a1e8af', '#94c595', '#747c92', '#372772', '#3a2449']
@@ -8,12 +9,13 @@ export default () => {
     <div className={styles.colors}>
       {colors.map((color, key) => {
         const colorName = ntc.name(color)[1]
+        const textColor = adjustTextColor(color)
 
         return (
           <div
             key={key}
             className={styles.colorWrapper}
-            style={{ backgroundColor: color }}
+            style={{ color: textColor, backgroundColor: color }}
           >
             <button
               className={styles.colorCode}
