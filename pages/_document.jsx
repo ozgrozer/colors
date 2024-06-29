20f22de64d5b989b3587337298ac440674c47d18
mac <ozgr@live.com>
2024-06-29 18:57:21 +0300

    Add poppins font

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,11 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 const MyDocument = () => {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap' rel='stylesheet' />
+      </Head>
       <body>
         <Main />
         <NextScript />
