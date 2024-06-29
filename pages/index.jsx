5d2385c2cf490f7988bf678ef2bf67590c7e32c4
mac <ozgr@live.com>
2024-06-29 17:48:02 +0300

    Initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default () => {
+  return (
+    <>
+      <Head>
+        <title>Colors</title>
+      </Head>
+
+      <div>Hello World!</div>
+    </>
+  )
+}
