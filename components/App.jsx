ae4c52616122e56b3f9be16b24862a470f50859c
mac <ozgr@live.com>
2024-06-29 21:37:58 +0300

    Separate colors component

--- components/App.jsx
+++ components/App.jsx
@@ -1,41 +1,10 @@
-import { useState, useEffect } from 'react'
-import niceColors from 'nice-color-palettes'
-
-import ntc from '@functions/ntc'
+import Colors from './Colors'
 import styles from '@styles/Colors.module.scss'
-import adjustTextColor from '@functions/adjustTextColor'
 
 export default () => {
-  const [colors, setColors] = useState([])
-
-  useEffect(() => {
-    setColors(niceColors[Math.floor(Math.random() * niceColors.length)])
-  }, [])
-
   return (
-    <div className={styles.colors}>
-      {colors.map((color, key) => {
-        const colorName = ntc.name(color)[1]
-        const textColor = adjustTextColor(color)
-
-        return (
-          <div
-            key={key}
-            className={styles.colorWrapper}
-            style={{ color: textColor, backgroundColor: color }}
-          >
-            <button
-              className={styles.colorCode}
-            >
-              {color.substr(1).toUpperCase()}
-            </button>
-
-            <div>
-              {colorName}
-            </div>
-          </div>
-        )
-      })}
+    <div className={styles.app}>
+      <Colors />
     </div>
   )
 }
