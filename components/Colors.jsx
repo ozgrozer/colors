0b037a3256edeb72424003983da172dab330716f
mac <ozgr@live.com>
2024-06-29 22:57:04 +0300

    Add new color buttons

--- components/Colors.jsx
+++ components/Colors.jsx
@@ -36,7 +36,7 @@ export default () => {
 
             {
               key !== colors.length - 1 && (
-                <button className={styles.addColorButton}>
+                <button className={styles.newColorButton}>
                   +
                 </button>
               )
