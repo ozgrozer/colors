3fcc460b8c98cf00b69bc42d3eadab07cdb2332d
mac <ozgr@live.com>
2024-06-29 23:50:38 +0300

    Add app context

--- components/App.jsx
+++ components/App.jsx
@@ -1,17 +1,20 @@
 import Header from './Header'
 import Colors from './Colors'
 import styles from '@styles/App.module.scss'
+import { AppProvider } from '@contexts/AppContext'
 
 export default () => {
   return (
-    <div className={styles.app}>
-      <div className={styles.headerWrapper}>
-        <Header />
-      </div>
+    <AppProvider>
+      <div className={styles.app}>
+        <div className={styles.headerWrapper}>
+          <Header />
+        </div>
 
-      <div className={styles.colorsWrapper}>
-        <Colors />
+        <div className={styles.colorsWrapper}>
+          <Colors />
+        </div>
       </div>
-    </div>
+    </AppProvider>
   )
 }
---
+++ contexts/AppContext.jsx
@@ -0,0 +1,31 @@
+import { useState, useContext, createContext } from 'react'
+
+const Context = createContext()
+
+export function AppProvider ({ children }) {
+  const [state, setState] = useState({
+    colors: []
+  })
+
+  const _setState = newState => {
+    setState(state => ({
+      ...state,
+      ...newState
+    }))
+  }
+
+  const value = {
+    state,
+    setState: _setState
+  }
+
+  return (
+    <Context.Provider value={value}>
+      {children}
+    </Context.Provider>
+  )
+}
+
+export function AppContext () {
+  return useContext(Context)
+}
