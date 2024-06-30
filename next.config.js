83c6defe30fdfb4f27d4609a60bd006daa317cb7
mac <ozgr@live.com>
2024-07-01 01:51:13 +0300

    Update urls

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicRuntimeConfig: {
-    developmentBaseUrl: 'http://localhost:3000/',
-    productionBaseUrl: 'https://ozgrozer.github.io/colors/'
+    developmentBaseUrl: 'http://localhost:3000',
+    productionBaseUrl: 'https://ozgrozer.github.io/colors'
   }
 }
