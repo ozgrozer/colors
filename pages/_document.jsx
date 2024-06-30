95c2527ec48a74d5cbaac2c69bb04931b198727a
mac <ozgr@live.com>
2024-06-30 15:17:59 +0300

    Fix cross origin

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,7 +5,7 @@ const MyDocument = () => {
     <Html>
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
         <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap' rel='stylesheet' />
       </Head>
       <body>
