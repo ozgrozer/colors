055312fcb6ca3af80c052660a27c37f235d5f891
mac <ozgr@live.com>
2024-06-30 16:24:09 +0300

    Add font-weight

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,7 @@ const MyDocument = () => {
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
-        <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap' rel='stylesheet' />
+        <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap' rel='stylesheet' />
       </Head>
       <body>
         <Main />
