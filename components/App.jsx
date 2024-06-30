f9c4551f53f66f17e9d6f35aa87a416b0a689490
mac <ozgr@live.com>
2024-06-30 23:27:13 +0300

    Get palettes from cookies on load

--- components/App.jsx
+++ components/App.jsx
@@ -1,20 +1,39 @@
+import { useState } from 'react'
+import { getCookie } from 'cookies-next'
+
 import Header from './Header'
 import Colors from './Colors'
 import styles from '@styles/App.module.scss'
-import { AppProvider } from '@contexts/AppContext'
+import { useAppContext, AppProvider } from '@contexts/AppContext'
+
+const App = ({ colors }) => {
+  const { setState } = useAppContext()
+
+  useState(() => {
+    const _getCookies = getCookie('palettes')
+    const palettes = _getCookies
+      ? JSON.parse(_getCookies)
+      : []
+    setState({ palettes })
+  }, [])
+
+  return (
+    <div className={styles.app}>
+      <div className={styles.headerWrapper}>
+        <Header />
+      </div>
+
+      <div className={styles.colorsWrapper}>
+        <Colors colors={colors} />
+      </div>
+    </div>
+  )
+}
 
 export default ({ colors }) => {
   return (
     <AppProvider>
-      <div className={styles.app}>
-        <div className={styles.headerWrapper}>
-          <Header />
-        </div>
-
-        <div className={styles.colorsWrapper}>
-          <Colors colors={colors} />
-        </div>
-      </div>
+      <App colors={colors} />
     </AppProvider>
   )
 }
